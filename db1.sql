2916a834583c28348e01c779ffcc8adcda64480c
Raluca <raluca.bulzan00@e-uvt.ro>
2022-11-20 14:48:14 +0200

    SQL script tip_parc table

---
+++ db1.sql
@@ -0,0 +1,19 @@
+--create tip_parc table
+CREATE TABLE IF NOT EXISTS tip_parc(
+	ID SERIAL PRIMARY KEY NOT NULL,
+	TYPE VARCHAR (20) NOT NULL,
+	description VARCHAR (400)
+);
+--INSERT DATA INTO TABLE AFTER CREATE
+INSERT INTO tip_parc(type, description) VALUES
+					('Parc national','Arie protejata infiintata conform regulament Romsilva'),
+					('Parc natural','Arie naturala protejata conform OUG Life Natura 2000'),
+					('Stiintifica','Rezervatie apartinand UVT, Departament Geografie'),
+					('Sit Natura 2000', 'Sit cartat si fundamentat conform Life Natura 200 Project');
+--UPDATE DATA IF REQUIRED
+UPDATE tip_parc SET 
+	type='Rez stiinfitica',
+	description= 'Rezervatie apartinand UVT, Departament Geografie & FMI'
+WHERE TYPE='Stiintifica';
+
+SELECT * FROM tip_parc;
